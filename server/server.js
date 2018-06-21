50679f6bb852485056e6ce8637924604ac867b15
תמיר דגני <degani.tamir@gmail.com>
2018-06-21 08:44:56 +0300

    'new_api'

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyparser = require('body-parser');
+var  ObjectID = require('mongodb').ObjectID;
 
 const port = process.env.PORT || 3000;
 
@@ -9,12 +10,13 @@ var {User} = require('./models/User');
 
 var app = express();
 app.use(bodyparser.json());
-
+//add new to do via api and add it to the mondo doc
 app.post('/todos', (req, res) => {
   console.log(req.body);
   var todo = new Todo({
     text: req.body.text
   });
+
 
   todo.save().then((doc) => {
     res.send(doc);
@@ -23,7 +25,59 @@ app.post('/todos', (req, res) => {
   });
 });
 
+//add user via api request
+app.post('/todos/adduser', (req, res) => {
+  console.log(req.body);
+  var user = new User({
+    email: req.body.email,
+    username: req.body.username
+  });
+
+
+  user.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+//get all user via api request
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+   res.send(e);
+ });
+})
+
+//get all users
+app.get('/users', (req, res) => {
+  User.find().then((todos) => {
+    res.send({users});
+  }, (e) => {
+   res.send(e);
+ });
+})
+
+
+//get username and print response all his info
+
+app.get('/users/:id', (req, res) => {
+var id = req.params.id;
+ if (!ObjectID.isValid(id)) {
+ return  res.status(404).send();
+ }
+User.findById(id).then((user) => {
+  res.send({user});
+}).catch((e) => {
+  res.status(400).send();
+})
+});
+
+
 
 app.listen(port, () => {
-  console.log('server is up');
+  console.log(port);
 });
+
+module.exports = {app};
