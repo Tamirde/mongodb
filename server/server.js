b1e72327aa0d8dd97ddf411589f91856d3a5626d
תמיר דגני <degani.tamir@gmail.com>
2018-06-21 11:00:05 +0300

    'herokuapi2'

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ var  ObjectID = require('mongodb').ObjectID;
 const port = process.env.PORT || 3000;
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/User');
+var {Todo} = require('./server/models/todo');
+var {User} = require('./server/models/User');
 
 var app = express();
 app.use(bodyparser.json());
